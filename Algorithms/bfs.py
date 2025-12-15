2597cdaa9d31bb0b58c6730af9882a06a0a78ead
Engy-Elyamany <engyelyamany26@gmail.com>
2025-12-15 14:47:25 +0200

    Improvements
    - use Algorithms.helper

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -1,16 +1,8 @@
 from collections import deque
+from Algorithms.helper import get_neighbors
 from config import *
 from Models.cell import *
 from Models.node import *
-
-# --- Get neighbors in 4 directions
-def get_neighbors(grid, cell):
-    neighbors = []
-    for d in [(-1,0),(1,0),(0,-1),(0,1)]:
-        r, c = cell.row + d[0], cell.col + d[1]
-        if 0 <= r < ROWS and 0 <= c < COLS and grid[r][c].color != "black":
-            neighbors.append(grid[r][c])
-    return neighbors
 
 # --- BFS Algorithm
 def bfs(draw, grid, start, end):
